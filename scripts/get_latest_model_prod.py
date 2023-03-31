e13dbcbdeb0add77d299fdbe314595004d3b5250
aurimasgriciunas <griciunas.aurimas@gmail.com>
2023-03-31 12:51:05 +0300

    Add more steps

---
+++ scripts/get_latest_model_prod.py
@@ -0,0 +1,29 @@
+import neptune
+from neptune.exceptions import ModelNotFound
+
+model_name = "pickled_model"
+
+# (Neptune) Get latest model from training stage
+model_key = "PIPELINES"
+project_key = "SHOW"
+
+try:
+    model = neptune.init_model(
+        with_id=f"{project_key}-{model_key}",  # Your model ID here
+    )
+    model_versions_table = model.fetch_model_versions_table().to_pandas()
+    production_model_table = model_versions_table[model_versions_table["sys/stage"] == "production"]
+    prod_model_id = production_model_table["sys/id"].tolist()[0]
+
+except ModelNotFound:
+    print(
+        f"The model with the provided key `{model_key}` doesn't exist in the `{project_key}` project."
+    )
+
+# (neptune) Download the lastest model checkpoint from model registry
+prod_model = neptune.init_model_version(with_id=prod_model_id)
+
+# (Neptune) Get model weights from training stage
+prod_model["model"][model_name].download()
+
+print(f"model to be deployed: {prod_model_id}. Model has been downloaded and is ready for deployment.")
