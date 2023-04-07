9599e6daf0b5c133f3dddb1535360189ceff9b48
aurimasgriciunas <griciunas.aurimas@gmail.com>
2023-04-07 13:39:37 +0300

    Deploy final pipeline

--- build_pipeline.py
+++ build_pipeline.py
@@ -26,6 +26,9 @@ ml_client = MLClient(
 
 from azure.ai.ml.entities import Data
 from azure.ai.ml.constants import AssetTypes
+from azure.ai.ml import command
+from azure.ai.ml import Input, Output
+
 
 web_path = "https://raw.githubusercontent.com/neptune-ai/examples/main/use-cases/time-series-forecasting/walmart-sales/dataset/aggregate_data.csv"
 
@@ -39,33 +42,15 @@ aggregate_data = Data(
 )
 
 aggregate_data = ml_client.data.create_or_update(aggregate_data)
+
 print(
     f"Dataset with name {aggregate_data.name} was registered to workspace, the dataset version is {aggregate_data.version}"
 )
-
-# web_path = "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
-
-# credit_data = Data(
-#     name="creditcard_defaults",
-#     path=web_path,
-#     type=AssetTypes.URI_FILE,
-#     description="Dataset for credit card defaults",
-#     tags={"source_type": "web", "source": "UCI ML Repo"},
-#     version="1.0.0",
-# )
-
-# credit_data = ml_client.data.create_or_update(credit_data)
-# print(
-#     f"Dataset with name {credit_data.name} was registered to workspace, the dataset version is {credit_data.version}"
-# )
 
 custom_env_name = "aml-scikit-learn"
 custom_env_version = "0.1.0"
 data_prep_src_dir = "components_2/data_prep"
 train_src_dir = "components_2/train"
-
-from azure.ai.ml import command
-from azure.ai.ml import Input, Output
 
 # data_prep_component = command(
 #     name="data_prep_credit_defaults",
@@ -93,18 +78,16 @@ data_prep_component = command(
     display_name="Data preparation for training",
     description="reads a .xl input, split the input to train and test",
     inputs={
-        "data": Input(type="uri_folder"),
-        # "test_train_ratio": Input(type="number"),
+        "data": Input(type="uri_folder")
     },
     outputs=dict(
-        train_data=Output(type="uri_folder", mode="rw_mount"),
-        valid_data=Output(type="uri_folder", mode="rw_mount"),
+        train_data=Output(type="uri_folder", mode="rw_mount")
     ),
     # The source folder of the component
     code=data_prep_src_dir,
     command="""python data_preprocessing.py \
             --data ${{inputs.data}} \
-            --train_data ${{outputs.train_data}} --valid_data ${{outputs.valid_data}}
+            --train_data ${{outputs.train_data}}
             """,
     environment=f"{custom_env_name}:{custom_env_version}",
 )
--- components_2/data_prep/data_preprocessing.py
+++ components_2/data_prep/data_preprocessing.py
@@ -1,50 +1,19 @@
-# %%
-# import os
-# from pathlib import Path
-
-# import matplotlib.pyplot as plt
-# import neptune
-
-# from mldesigner import Input, Output, command_component
-# from neptune.integrations.xgboost import NeptuneCallback
 from utils import *
 import argparse
 
 
-# @command_component(
-#     name="prepare_data",
-#     display_name="Prepare data",
-#     description="Prepare data for training",
-#     version="0.1",
-#     environment=dict(
-#         conda=Path(__file__).parent / "conda.yaml",
-#         image="mcr.microsoft.com/azureml/openmpi4.1.0-ubuntu20.04",
-#     ),
-# )
-def data_preprocessing_component(
-    dataset_path: str,
-    train_data: str,
-    valid_data: str,
-    # dataset: Input(type="uri_folder", description="Dataset"),
-    # train_data: Output(type="uri_folder", description="Train data"),
-    # valid_data: Output(type="uri_folder", description="Validation data"),
-):
+def data_preprocessing_component():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--data", type=str, help="path to input data")
     parser.add_argument("--train_data", type=str, help="path to train data")
-    parser.add_argument("--test_data", type=str, help="path to test data")
     args = parser.parse_args()
 
-    # (neptune) Initialize Neptune run
-    # run = neptune.init_run(
-    #     tags=["baseline", "xgboost", "walmart-sales"],
-    #     name="XGBoost",
-    # )
-
-    # Load dataset
+    # # Load dataset
     df = pd.read_csv(args.data)
-    # load_data(args.data, cache=True)
+
+    # Drop index
+    df = df.drop("Column1", axis=1)
 
     # Normalize sales data
     df_normalized = normalize_data(df, "Weekly_Sales")
@@ -56,30 +25,12 @@ def data_preprocessing_component(
     # Create Lagged features
     df_encoded = create_lags(df_encoded)
 
-    # Split data into train and validation
-    X_train, X_valid, y_train, y_valid = get_train_data(df_encoded)
-
-    # Concatenate x and y train data
-    train_df = pd.concat([X_train, y_train], axis=1)
-
-    # Concatenate x and y validation data
-    valid_df = pd.concat([X_valid, y_valid], axis=1)
-
     # Save train and validation data
-    TRAIN_DATA_PATH = os.path.join(args.train_data, "train_data.csv")
-    VALID_DATA_PATH = os.path.join(args.valid_data, "validation_data.csv")
+    train_data_path = os.path.join(args.train_data, "train_data.csv")
+    df_encoded.to_csv(train_data_path, index=False)
 
-    train_df.to_csv(os.path.join(TRAIN_DATA_PATH, "train_data.csv"), index=False)
-
-    valid_df.to_csv(os.path.join(VALID_DATA_PATH, "validation_data.csv"), index=False)
-
-    # train_df.to_csv(TRAIN_DATA_PATH, index=False)
-    # valid_df.to_csv(VALID_DATA_PATH, index=False)
+    print(df_encoded)
 
 
 if __name__ == "__main__":
-    data_preprocessing_component(
-        dataset_path="../data",
-        train_data="../data",
-        valid_data="../data",
-    )
+    data_preprocessing_component()
